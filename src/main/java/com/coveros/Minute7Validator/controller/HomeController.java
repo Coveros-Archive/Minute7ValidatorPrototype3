551d3ca6dc8b8751b7659aa019fbd033a1be6baa
Devin Lee <jy4ny@virginia.edu>
2015-12-01 11:19:33 -0500

    Spring Security implemented. SMTP implemented

--- src/main/java/com/coveros/Minute7Validator/controller/HomeController.java
+++ src/main/java/com/coveros/Minute7Validator/controller/HomeController.java
@@ -18,10 +18,13 @@ import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coveros.Minute7Validator.library.DataImporter;
 import com.coveros.Minute7Validator.library.NotificationManager;
@@ -40,8 +43,10 @@ public class HomeController {
 	private static ArrayList<TimecardEntry> timecardEntries;
 	private static Hashtable<String, Employee> employeeDirectory;
 	@Autowired
-    ApplicationContext context;
-	//private @Value("#{minute7.accountName}") String m7accountName;
+	ApplicationContext context;
+	@Autowired
+	private JavaMailSender mailSender;
+	// private @Value("#{minute7.accountName}") String m7accountName;
 
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -62,43 +67,78 @@ public class HomeController {
 
 	/**
 	 * Simply selects the test view to render by returning its name.
-	 * @throws IOException 
-	 * @throws ParseException 
+	 * 
+	 * @throws IOException
+	 * @throws ParseException
 	 */
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test(Locale locale, Model model) throws IOException, ParseException {
 		Properties appProp = (Properties) context.getBean("myPropertiesBean");
-		String filePathTimecardEntries= System.getProperty("user.home") + "\\data\\timecardEntries\\coveros_inc_time_export.xls.txt";
-		String filePathDependencies= System.getProperty("user.home") + "\\data\\dependencies\\M7 Entry Dependency Model.xlsx";
-		String filePathEmails=System.getProperty("user.home") + "\\data\\directory\\coveros_directory.xlsx";
+		String filePathTimecardEntries = System.getProperty("user.home")
+				+ "\\data\\timecardEntries\\coveros_inc_time_export.xls.txt";
+		String filePathDependencies = System.getProperty("user.home")
+				+ "\\data\\dependencies\\M7 Entry Dependency Model.xlsx";
+		String filePathEmails = System.getProperty("user.home") + "\\data\\directory\\coveros_directory.xlsx";
 		System.out.println("Loading application...");
-		jobList=new Hashtable<String, Job>();
-		timecardEntries=new ArrayList<TimecardEntry>();
-		employeeDirectory=new Hashtable<String, Employee>();
-		
-		//Initialize data importer
-		DataImporter di=new DataImporter(logger, appProp);
-		//Initialize validator
+		jobList = new Hashtable<String, Job>();
+		timecardEntries = new ArrayList<TimecardEntry>();
+		employeeDirectory = new Hashtable<String, Employee>();
+
+		// Initialize data importer
+		DataImporter di = new DataImporter(logger, appProp);
+		// Initialize validator
 		Validator v = new Validator();
-		//initialize notification manager
-		NotificationManager nm=new NotificationManager();
-		
-		//Purge previous timecard data if any
+		// initialize notification manager
+		NotificationManager nm = new NotificationManager();
+
+		// Purge previous timecard data if any
 		di.purgeDataFolder(new File(System.getProperty("user.home") + "\\data\\timecardEntries"));
-		
-		//Use Selenium to download the timecard file
-		filePathTimecardEntries= di.downloadTimecardDataWithSelenium();
-		
+
+		// Use Selenium to download the timecard file
+		filePathTimecardEntries = di.downloadTimecardDataWithSelenium();
+
 		di.importDependencies(filePathDependencies, jobList);
-		//di.importTimeEntries(filePathTimecardEntries, jobList, timecardEntries);
+		// di.importTimeEntries(filePathTimecardEntries, jobList,
+		// timecardEntries);
 		di.importTimecardEntriesTabDelimitedTXT(filePathTimecardEntries, timecardEntries);
-		//di.importEmails(filePathEmails, employeeDirectory);
-		
-		
+		// di.importEmails(filePathEmails, employeeDirectory);
+
 		v.validateTimcardEntries(jobList, timecardEntries);
-		
+
 		di.purgeDataFolder(new File(System.getProperty("user.home") + "\\data\\timecardEntries"));
 		return "test";
+	}
+
+	/**
+	 * Test for sending email
+	 */
+	@RequestMapping(value = "/email", method = RequestMethod.GET)
+	public String emailTest(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		Properties appProp = (Properties) context.getBean("myPropertiesBean");
+		String aaa = appProp.getProperty("gmail.accountName");
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setTo("jy4ny@virginia.edu");
+		email.setSubject("FAFAFAFAF");
+		email.setText("greetings!");
+		mailSender.send(email);
+
+		return "home";
+	}
+
+	// Spring Security see this :
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		return "login";
+
 	}
 
 }
